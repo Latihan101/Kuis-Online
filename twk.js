c0fb3e823b6fa7edfc4de1c4f44b32cd05a024c8
Latihan101 <yikapps98@gmail.com>
2026-06-12 07:30:55 +0700

    Create twk.js

---
+++ twk.js
@@ -0,0 +1,21 @@
+var materiTWK = {
+  judul: "TWK - Tes Wawasan Kebangsaan",
+  konten: `
+    <h3>📚 Nasionalisme</h3>
+    <p>Nasionalisme adalah paham kebangsaan yang mengandung makna kesadaran dan semangat cinta tanah air...</p>
+    
+    <h3>📚 Integritas</h3>
+    <p>Integritas adalah kualitas kejujuran dan prinsip moral yang kuat...</p>
+    
+    <h3>📚 Bela Negara</h3>
+    <p>Bela negara adalah sikap dan perilaku warga negara yang dijiwai oleh kecintaannya kepada NKRI...</p>
+    
+    <h3>📚 Pilar Negara</h3>
+    <ul>
+      <li><strong>Pancasila</strong> - Dasar negara Indonesia</li>
+      <li><strong>UUD 1945</strong> - Konstitusi negara</li>
+      <li><strong>NKRI</strong> - Bentuk negara kesatuan</li>
+      <li><strong>Bhinneka Tunggal Ika</strong> - Semboyan persatuan</li>
+    </ul>
+  `
+};
