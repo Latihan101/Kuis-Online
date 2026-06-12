fefbce44c916fdd4e869af0ecedc6bf50414de45
Latihan101 <yikapps98@gmail.com>
2026-06-12 07:32:03 +0700

    Create tkp.js

---
+++ tkp.js
@@ -0,0 +1,16 @@
+var materiTKP = {
+  judul: "TKP - Tes Karakteristik Pribadi",
+  konten: `
+    <h3>⭐ Pelayanan Publik</h3>
+    <p>Kemampuan memberikan pelayanan terbaik kepada masyarakat...</p>
+    
+    <h3>⭐ Jejaring Kerja</h3>
+    <p>Kemampuan membangun dan memelihara hubungan kerja yang efektif...</p>
+    
+    <h3>⭐ Sosial Budaya</h3>
+    <p>Kemampuan beradaptasi dan bekerja dalam keberagaman...</p>
+    
+    <h3>⭐ Teknologi Informasi & Komunikasi</h3>
+    <p>Kemampuan memanfaatkan teknologi untuk meningkatkan produktivitas...</p>
+  `
+};
