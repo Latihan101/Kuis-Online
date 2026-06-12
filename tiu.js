b35015b80fcb4b22bb72ebc1b7e7f4d4c28d4d90
Latihan101 <yikapps98@gmail.com>
2026-06-12 07:31:25 +0700

    Create tiu.js

---
+++ tiu.js
@@ -0,0 +1,13 @@
+var materiTIU = {
+  judul: "TIU - Tes Intelegensi Umum",
+  konten: `
+    <h3>🔢 Kemampuan Verbal</h3>
+    <p>Meliputi sinonim, antonim, analogi, dan pemahaman wacana...</p>
+    
+    <h3>🔢 Kemampuan Numerik</h3>
+    <p>Meliputi aritmatika, deret angka, perbandingan, dan soal cerita...</p>
+    
+    <h3>🔢 Kemampuan Figural</h3>
+    <p>Meliputi pola gambar, serial, dan klasifikasi gambar...</p>
+  `
+};
