a167330fe182f5cf635e83c68f0d87f3317617c6
Latihan101 <yikapps98@gmail.com>
2026-06-13 17:15:27 +0700

    Update quiztiu.js

--- quiztiu.js
+++ quiztiu.js
@@ -1,211 +1,602 @@
 var quizTIU= [
-    { // No 31
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 32
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 33
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 34
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 35
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 36
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
-    pembahasan: ""
-  },
-  { // No 37
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 38
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 39
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 40
-    q: "",
-    opt: ["","","","",""],
-    a: 2,
-    pembahasan: ""
-  },
-  { // No 41
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 42
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 43
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 44
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 45
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 46
-    q: "",
-    opt: ["","","","",""],
-    a: 2,
-    pembahasan: ""
-  },
-  { // No 47
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 48
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 49
-    q: "",
-    opt: ["","","","",""],
-    a: 2,
-    pembahasan: ""
-  },
-  { // No 50
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
-    pembahasan: ""
-  },
+  SPASIAL = { // No 1
+  q: "ANALOGI =",
+  opt: ["Tersusun","Perbandingan","Pengembangan","Telaah","Menguraikan"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 2
+  q: "SYAHDU =",
+  opt: ["Indah","Elok","Khidmat","Megah","Surgawi"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 3
+  q: "ONAR =",
+  opt: ["Angkara","Murka","Dendam","Khianat","Rusak"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 4
+  q: "MENDEPAK =",
+  opt: ["Menghalau","Mengurangi","Menyingkir","Menyepak","Memundurkan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 5
+  q: "KONTUR =",
+  opt: ["Patahan","Lengkung","Sungkup","Permukaan","Siluet"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 6
+  q: "PIONIR =",
+  opt: ["Praktisi","Aktivis","Pembaru","Pemandu","Pemula"],
+  a: 4,
+  pembahasan: ""
+},
+{ // No 7
+  q: "PEMEO =",
+  opt: ["Ibarat kata","Sandi","Pertanda","Semboyan","Pengandaian"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 8
+  q: "SETERU =",
+  opt: ["Perlawanan","Tanding","Selisih","Adu","Oponen"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 9
+  q: "KUALIFIKASI =",
+  opt: ["Nilai","Kompetensi","Patokan","Taraf","Tingkat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 10
+  q: "PERAMBAN =",
+  opt: ["Pangkalan","Penjelajah","Jaringan","Jejaring","Peranti"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 11
+  q: "LEGIUN =",
+  opt: ["Gabungan","Kavaleri","Sipil","Armada","Pertahanan"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 12
+  q: "NALAR =",
+  opt: ["Rasio","Nilai","Asumsi","Logis","Alat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 13
+  q: "OBSESI =",
+  opt: ["Hasrat","Keterikatan","Dorongan","Animo","Mania"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 14
+  q: "INKUBASI =",
+  opt: ["Pengembangbiakan","Masa tunas","Pembiakan","Laju sel","Pengubahan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 15
+  q: "TRANSISI =",
+  opt: ["Pemindahan","Penyaringan","Penyeleksian","Perlintasan","Perantara"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 16
+  q: "FEODAL =",
+  opt: ["Pribumi","Konservatif","Modern","Berkembang","Terpadu"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 17
+  q: "ELUSIF =",
+  opt: ["Terkaan","Sandi","Misterius","Dugaan","Terpencil"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 18
+  q: "EKSENTRIK =",
+  opt: ["Moderat","Kontemporer","Biasa","Abnormal","Umum"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 19
+  q: "STRATEGIS =",
+  opt: ["Terencana","Taktis","Prosedur","Utama","Desain"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 20
+  q: "GELAGAT =",
+  opt: ["Penanda","Perangai","Indikasi","Karakteristik","Pola"],
+  a: 4,
+  pembahasan: ""
+},
+{ // No 21
+  q: "MARGIN =",
+  opt: ["Sisi","Lingkup","Sudut","Titik","Batas"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 22
+  q: "FRAGMEN =",
+  opt: ["Penggalan","Bagian","Sisi","Nukilan","Rangkaian"],
+  a: 4,
+  pembahasan: ""
+},
+{ // No 23
+  q: "HISTERIA =",
+  opt: ["Waswas","Fanatik","Guncangan","Ketakutan","Histeris"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 24
+  q: "DISKREDIT =",
+  opt: ["Menghalau","Menepikan","Meminggirkan","Memojokkan","Merusak"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 25
+  q: "AFIKS =",
+  opt: ["Ujaran","Imbuhan","Asosiasi","Penanda","Hubungan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 26
+  q: "DOMAIN =",
+  opt: ["Lingkup","Titik","Jangkauan","Lintasan","Ranah"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 27
+  q: "KONGSI =",
+  opt: ["Komunitas","Aliansi","Perseoran","Ikatan","Organisasi"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 28
+  q: "KONTAMINASI =",
+  opt: ["Paparan","Radiasi","Buangan","Pencemaran","Limbah"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 29
+  q: "LIGA =",
+  opt: ["Kelompok","Perserikatan","Satuan","Bagian","Badan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 30
+  q: "SUPERVISI =",
+  opt: ["Penyeliaan","Pengadaan","Pemeriksaan","Pengujian","Pengesahan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 31
+  q: "SELONGSONG =",
+  opt: ["Sebotir","Sepasang","Peredam","Selubung","Penapis"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 32
+  q: "EKSAK =",
+  opt: ["Nyata","Tunggal","Sesuai","Tepat","Terbukti"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 33
+  q: "GLOSARI =",
+  opt: ["Istilah","Kosakata","Daftar","Penjelasan","Rangkuman"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 34
+  q: "INTI SARI =",
+  opt: ["Dasar","Substansi","Ilmu","Kajian","Daftar"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 35
+  q: "MEMBRAN =",
+  opt: ["Plasma","Inti","Selaput","Kelenjar","Saluran"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 36
+  q: "EKSKRESI =",
+  opt: ["Penyaringan","Pemisahan","Pengeluaran","Penjernihan","Pelarutan"],
+  a: 4,
+  pembahasan: ""
+},
+{ // No 37
+  q: "RENIK =",
+  opt: ["Kecil","Bernyawa","Rendah","Remeh","Rentan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 38
+  q: "KELIR =",
+  opt: ["Selubung","Jalur","Pemisah","Layar","Penahan"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 39
+  q: "AKSEPTOR =",
+  opt: ["Awam","Khalayak","Golongan","Penduduk","Penerima"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 40
+  q: "EKUIVALEN =",
+  opt: ["Selaras","Seimbang","Sempurna","Sebanding","Sepadan"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 41
+  q: "ESENSI =",
+  opt: ["Zat","Hakikat","Ekstrak","Bahan","Unsur"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 42
+  q: "FEDERASI =",
+  opt: ["Badan","Satuan","Uni","Lembaga","Bagian"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 43
+  q: "VITALITAS =",
+  opt: ["Gembira","Energi","Riang","Berani","Gagah"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 44
+  q: "PARTIKELIR =",
+  opt: ["Singular","Badan","Swasta","Unit","Golongan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 45
+  q: "MUKADIMAH =",
+  opt: ["Prapendapat","Keterangan","Salam","Bagian","Pendahuluan"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 46
+  q: "ASTA =",
+  opt: ["Bagian","Delapan","Bilangan","Alfabet","Satuan"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 47
+  q: "ENIGMA =",
+  opt: ["Klinik","Astral","Bayangan","Misteri","Semesta"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 48
+  q: "NISBAH =",
+  opt: ["Angka","Bilangan","Skala","Pecahan","Pembilang"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 49
+  q: "SAHAYA =",
+  opt: ["Pegawai","Abdi","Pekerja","Rakyat","Jelata"],
+  a: 2,
+  pembahasan: ""
+},
+{ // No 50
+  q: "KOMPATIBEL =",
+  opt: ["Serasi","Bertalian","Nyata","Pas","Diterima"],
+  a: 4,
+  pembahasan: ""
+},
   { // No 51
-    q: "",
-    opt: ["","","","",""],
-    a: 3,
-    pembahasan: ""
-  },
-  { // No 52
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
-    pembahasan: ""
-  },
-  { // No 53
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 54
-    q: "",
-    opt: ["","","","",""],
-    a: 0,
-    pembahasan: ""
-  },
-  { // No 55
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 56
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 57
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 58
-    q: "",
-	  a:1,
-	  pembahasan: ""
-  },
-  { // No 59
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 60
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 61
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 62
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 63
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 64
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  },
-  { // No 65
-    q: "",
-    opt: ["","","","",""],
-    a: 1,
-    pembahasan: ""
-  } 
-  ];
+  q: "FLEKSIBEL ><",
+  opt: ["Luwes","Tak pilih-pilih","Selaras","Stagnan","Kaku"],
+  a: 3,
+  pembahasan: ""
+},
+{ // No 52
+  q: "APATIS ><",
+  opt: ["Ragu","Tak acuh","Objektif","Tegas","Peduli"],
+  a: 4,
+  pembahasan: ""
+},
+{ // No 53
+  q: "SUBORDINAT ><",
+  opt: ["Sekunder","Singular","Tunggal","Sumbu","Bawahan"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 54
+  q: "VARIA ><",
+  opt: ["Tunggal","Jamak","Jenis","Kumpulan","Soliter"],
+  a: 0,
+  pembahasan: ""
+},
+{ // No 55
+  q: "POLEMIK ><",
+  opt: ["Rasional","Aksioma","Selaras","Selisih","Berkaitan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 56
+  q: "KONTRA ><",
+  opt: ["Terbuka","Memihak","Berbeda","Menyatu","Bersisian"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 57
+  q: "SKEPTIS ><",
+  opt: ["Waspada","Curiga","Yakin","Tak acuh","Kritis"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 58
+  q: "JASMANI ><",
+  opt: ["Badani","Fisik","Ragawi","Sukma","Substansi"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 59
+  q: "GEGARAH ><",
+  opt: ["Teledan","Rajin","Cepat","Tangkas","Lekas"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 60
+  q: "HETEROGEN ><",
+  opt: ["Ragam","Varian","Jamak","Rangkap","Tunggal"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 61
+  q: "DAIF ><",
+  opt: ["Kurang","Mulia","Bernilai","Murni","Cela"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 62
+  q: "INVALID ><",
+  opt: ["Fit","Bugar","Segar","Mandiri","Normal"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 63
+  q: "ABSAH ><",
+  opt: ["Terlarang","Sahih","Cacat","Ilegal","Benar"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 64
+  q: "INFERTIL ><",
+  opt: ["Normal","Subur","Cacat","Sehat","Baik"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 65
+  q: "IMBESIL ><",
+  opt: ["Tumpul","Lambat","Spesial","Pandai","Sukar"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 66
+  q: "NATALITAS ><",
+  opt: ["Ketiadaan","Kesuburan","Kelajuan","Kemunduran","Kematian"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 67
+  q: "SPASIAL ><",
+  opt: ["Ruang","Universal","Terbatas","Negeri","Daerah"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 68
+  q: "PLURAL ><",
+  opt: ["Jamak","Satuan","Tunggal","Padu","Pertama"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 69
+  q: "SUSILA ><",
+  opt: ["Terbuka","Vulgar","Adab","Norma","Tatanan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 70
+  q: "LESTARI ><",
+  opt: ["Sementara","Langgeng","Hampa","Sunyi","Lengang"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 71
+  q: "EKSISTENSI ><",
+  opt: ["Kefanaan","Kehampaan","Ketiadaan","Kekosongan","Kesunyian"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 72
+  q: "BACIN ><",
+  opt: ["Segar","Wangi","Anyir","Asam","Bau"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 73
+  q: "DEFISIT ><",
+  opt: ["Anggaran","Kekurangan","Ganjil","Surplus","Sedikit"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 74
+  q: "DEGIL ><",
+  opt: ["Tengil","Kumal","Pengikut","Penurut","Normal"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 75
+  q: "ANOMALI ><",
+  opt: ["Biasa","Sederhana","Normal","Nyata","Kurang"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 76
+  q: "DEGENERASI ><",
+  opt: ["Kemajuan","Kesetaraan","Ketimpangan","Keseimbangan","Keselarasan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 77
+  q: "NYATA ><",
+  opt: ["Visual","Delusi","Terbukti","Ada","Benar"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 78
+  q: "PROFIT ><",
+  opt: ["Uang","Hasil","Bangkrut","Laba","Rugi"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 79
+  q: "BELIA ><",
+  opt: ["Segar","Ringkih","Kisut","Uzur","Sakit"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 80
+  q: "RONGSOK ><",
+  opt: ["Modern","Maju","Normal","Baru","Baik"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 81
+  q: "MUTLAK ><",
+  opt: ["Pasti","Yakin","Nisbi","Ukuran","Absolut"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 82
+  q: "INTIM ><",
+  opt: ["Pasif","Karib","Celah","Renggang","Erat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 83
+  q: "AKTUAL ><",
+  opt: ["Lama","Terbaru","Kekal","Senantiasa","Modern"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 84
+  q: "ABSURD ><",
+  opt: ["Lain","Berbeda","Asing","Luar biasa","Normal"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 85
+  q: "TUNGGAL ><",
+  opt: ["Padu","Varia","Manunggal","Sejenis","Sedikit"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 86
+  q: "GASAL ><",
+  opt: ["Tepat","Genap","Cocok","Lengkap","Cukup"],
+  a: 1,
+  pembahasan: ""
+},
+  { // No 87
+  q: "RIVAL ><",
+  opt: ["Protagonis","Oponen","Pihak","Perantara","Musuh"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 88
+  q: "MENJAMIN ><",
+  opt: ["Meminta","Mengambil","Menanggung","Menebus","Menjanjikan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 89
+  q: "CEMERLANG ><",
+  opt: ["Gemilang","Suram","Sirna","Pudar","Hilang"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 90
+  q: "PROVOKATOR ><",
+  opt: ["Pendamai","Partisipan","Penolong","Pendukung","Panutan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 91
+  q: "SINTETIS ><",
+  opt: ["Sejati","Suci","Polos","Asli","Murni"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 92
+  q: "EKSPLISIT ><",
+  opt: ["Jelas","Mudah","Gamblang","Gambaran","Tersirat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 93
+  q: "IMPULS ><",
+  opt: ["Cepat","Stimulan","Langsung","Penolakan","Emosional"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 94
+  q: "MUBAZIR ><",
+  opt: ["Royal","Efisien","Tepat","Jitu","Cermat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 95
+  q: "MUDARAT ><",
+  opt: ["Manjur","Buruk","Saksama","Faedah","Hemat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 96
+  q: "IMPRESIF ><",
+  opt: ["Biasa","Sederhana","Umum","Sering","Berulang"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 97
+  q: "CIUT ><",
+  opt: ["Besar","Kuat","Melar","Padat","Hebat"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 98
+  q: "JAWARA ><",
+  opt: ["Unggul","Pecundang","Menyerah","Musuh","Tandingan"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 99
+  q: "ELASTIS ><",
+  opt: ["Kaku","Fleksibel","Mudah","Lentur","Lekuk"],
+  a: 1,
+  pembahasan: ""
+},
+{ // No 100
+  q: "KOALISI ><",
+  opt: ["Perceraian","Pemisahan","Pemutusan","Perpisahan","Pengasingan"],
+  a: 1,
+  pembahasan: ""
+}
+];
