45d1249a9e43cf8fca29ac92ed33858ba32068bc
Latihan101 <yikapps98@gmail.com>
2026-06-14 18:10:29 +0700

    Update quiztwk.js

--- quiztwk.js
+++ quiztwk.js
@@ -1,182 +1,182 @@
 var quizTWK= [
   { // No 1
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Makna dari semboyan Bhinneka Tunggal Ika adalah ...",
+    opt: ["berbeda-beda suku bangsa","bersama dalam kemajemukan","bersama dalam perbedaan kebudayaan","berbeda-beda, tetapi tetap satu","berbeda-beda dalam keragaman"],
+    a: 3,
     pembahasan: ""
   },
   { // No 2
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Lambang Negara Indonesia adalah Garuda Indonesia dengan semboyan Bhinneka Tunggallka, sebagaimana tercantum dalam UUD 1945 Pasal 36 A yang merupakan hasil  amandemen ...",
+    opt: ["kesatu","kedua","ketiga","keempat","kelima"],
+    a: 1,
     pembahasan: ""
   },
   { // No 3
-    q: "",
-    opt: ["","","","",""],
+    q: "Masyarakat Indonesia memiliki kearifan lokal yang beragam pada tiap daerah.Tiap daerah memiliki pola hidup yang berbeda, misalnya dalam kegiatan ekonomi ada yang mengembangkan sektor pertanian perikanan dan perkebunan. Kondisi tersebut dipengaruhi oleh faktor ...",
+    opt: ["tradisi masyarakat setempat","lapangan pekerjaan","sempitnya daya manusia","keterampilan masyarakat","faktor geografis"],
     a: 4,
     pembahasan: ""
   },
   { // No 4
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Keragaman masyarakat Indonesia berdasarkan kriteria agama ditandai dengan ...",
+    opt: ["terjadinya konflik antarumat beragama","terdapat kebebasan dalam kegiatan dakwah atau ceramah keagamaan","diakuinya berbagai agama dan para pemeluknya","terdapat salah satu agama mayoritas di tengah-tengah masyarakat","munculnya aliran kepercayaan yang beragam di dalam masyarakat"],
+    a: 2,
     pembahasan: ""
   },
   { // No 5
-    q: "",
-    opt: ["","","","",""],
+    q: "Bentuk wilayah Indonesia yang terdiri atas lautan dengan ribuan pulau yang terbentang dari Sabang sampai Merauke telah melatarbelakangi terjadinya kemajemukan suku bangsa sebab ...",
+    opt: ["terdapat kesenjangan sosial antarwilayah","terdapat perbedaan iklim","terdapat kesenjangan budaya antar daerah","tersendatnya komunikasi antarwilayah","antarwilayah terjadi isolasi geografis"],
     a: 4,
     pembahasan: ""
   },
   { // No 6
-    q: "",
-    opt: ["","","","",""],
+    q: "Pernyataan berikut merupakan penyebab keanekaragaman agama di Indonesia, kecuali ...",
+    opt: ["pengaruh budaya asing","pada masa lampau Indonesia merupakan jalur perdagangan","letak Indonesia yang strategis sehingga mendorong bangsa asing untuk datang.","kondisi Indonesia yang terdiri atas ribuan pulau","perbedaan kondisi geografi"],
     a: 4,
     pembahasan: ""
   },
   { // No 7
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Kondisi masyarakat Indonesia yang terdiri atas beragam suku bangsa dapat menyebabkan konflik antarsuku bangsa yang didorong oleh etnosentrisme. Hal tersebut akibat dari perbedaan ...",
+    opt: ["budaya","keturunan","pekerjaan","agama","ekonomi"],
+    a: 0,
     pembahasan: ""
   },
   { // No 8
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Apabila di dalam masyarakat terjadi konflik yang tidak dapat teratasi, maka pada puncaknya akan terjadi ...",
+    opt: ["akulturasi","asimilasi","koersi","disintegrasi","akomodasi"],
+    a: 3,
     pembahasan: ""
   },
   { // No 9
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Proses peleburan dua atau lebih kebudayaan yang berbeda menjadi satu kebudayaan tunggal yang dirasakan sebagai kebudayaan milik bersama merupakan proses ...",
+    opt: ["akulturasi","amalgamasi","asimilasi","akomodasi","primordialisme"],
+    a: 2,
     pembahasan: ""
   },
   { // No 10
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Masyarakat Indonesia yang majemuk memiliki potensi terjadinya konflik akibat keberagaman perbedaan. Konflik dalam masyarakat tersebut dapat diminimalisasi apabila perbedaan yang ada di dalam masyarakat dilandasi dengan sikaр ...",
+    opt: ["loyalitas yang berlebihan terhadap agama, keluarga, dan suku bangsa","mengunggulkan kebudayaan masyarakat tertentu","saling menghormati dan menghargai perbedaan antarwarga masyarakat","saling menghormati dan menghargai intern kelompok","mengembangkan sikap toleransi terhadap satu kelompok agama tertentu"],
+    a: 2,
     pembahasan: ""
   },
   { // No 11
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Alasan semangat nasionalisme diperlukan dalam kehidupan masyarakat majemuk adalah ...",
+    opt: ["Nasionalisme sebagai landasan untuk terciptanya bangsa yang berdaulat dan bersatu","Nasionalisme dapat menguatkan keyakinan dalam menghadapi ancaman dari luar","Tanpa semangat nasionalisme, bangsa kita akan terkena dampak negatif globalisasi","Tanpa semangat nasionalisme, bangsa kita tidak punya pegangan dalam menghadapi proses globalisasi.","Nasionalisme dapat mendorong munculnya kearifan lokal pada tiap-tiap daerah bersatu"],
+    a: 0,
     pembahasan: ""
   },
   { // No 12
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Sikap saling menghargai, toleransi terhadap perbedaan masyarakat, dan tidak mengunggulkan kebudayaan masyarakat tertentu memiliki dampak positif bagi kehidupan suatu bangsa, yaitu ...",
+    opt: ["mendorong terbentuknya pertentangan dalam masyarakat","wibawa pemerintah akan tetap terjaga di mata masyarakat nasional maupun internasional","integrasi bangsa akan tetap terjaga","menghambat proses asimilasi ataupun akulturasi dalam masyarakat","menghapus kesenjangan budaya dalam masyarakat"],
+    a: 2,
     pembahasan: ""
   },
   { // No 13
-    q: "",
-    opt: ["","","","",""],
+    q: "Bangsa Indonesia merupakan bangsa yang majemuk. Meskipun demikian, masyarakat dapat bersatu dalam kemajemukan sosial budaya karena memiliki ...",
+    opt: ["sifat primordialisme","bahasa daerah yang sama","kebudayaan yang sama","asal usul dan ciri fisik yang sama","ideologi yang sama"],
     a: 4,
     pembahasan: ""
   },
   { // No 14
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Salah satu konsekuensi dalam masyarakat multikultural adalah nasionalisme. Hal tersebut sesuai dengan pernyataan ...",
+    opt: ["perekrutan pegawai berdasarkan kesamaan daerah","mengutamakan penggunaan bahasa Indonesia ketika berada dalam forum nasional","mengunggulkan kebudayaan sendiri dibandingkan dengan kebudayaan lain","bekerja sama dengan didasari kesamaan budaya","membuat perkumpulan dengan dilatarbelakangi sikap primordialisme"],
+    a: 1,
     pembahasan: ""
   },
   { // No 15
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: " Perhatikan pernyataan-pernyataan berikut: (1) kurang mengedepankan musyawarah untuk mencapai mufakat; (2) ada kecenderungan melakukan dominasi terhadap suatu kelompok; (3) memiliki karakter yang hampir sama dan seragam; (4) integrasi terjadi karena kesadaran masyarakat. Ciri masyarakat multikultural ditunjukkan oleh nomor ...",
+    opt: ["(1) dan (2)","(1) dan (3)","(1) dan (4)","(2) dan (3)","(2) dan (4)"],
+    a: 0,
     pembahasan: ""
   },
   { // No 16
-    q: "",
-    opt: ["","","","",""],
+    q: "Sikap menghargai perbedaan suku bangsa ditunjukkan oleh pernyataan ...",
+    opt: ["melakukan diskriminasi terhadap kebudayaan suku tertentu","mengunggulkan suku bangsanya sendiri.","meremehkan budaya yang berasal dari masyarakat lain","memberikan penghargaan terhadap kebudayaan yang berasal dari kebudayaan sendiri","memberikan apresiasi terhadap pertunjukan kesenian dari kebudayaan lain"],
     a: 4,
     pembahasan: ""
   },
   { // No 17
-    q: "",
-    opt: ["","","","",""],
+    q: "Integrasi nasional dapat diwujudkan dalam kehidupan masyarakat majemuk apabila ...",
+    opt: ["cara berpikir anggota masyarakat relatif sama","gaya hidup yang seragam","setiap individu lebih mengutamakan haknya","memiliki pemimpin yang bijaksana dan merakyat","terdapat rasa cinta Tanah Air pada setiap warga Negara Indonesia"],
     a: 4,
     pembahasan: ""
   },
   { // No 18
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Integrasi dapat terwujud dalam masyarakat apabila dilandasi oleh ...",
+    opt: ["semangat untuk saling mempertentangkan budaya daerah","keinginan untuk saling berkompetisi secara sehat","keinginan untuk tampak menonjol di mata masyarakat lainnya","adanya semangat untuk menciptakan persatuan dan kesatuan","adanya semangat untuk membela kelompok masyarakat"],
+    a: 3,
     pembahasan: ""
   },
   { // No 19
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Penggunaan bahasa Indonesia dapat menjadi salah satu faktor terbentuknya integrasi dalam masyarakat. Bahasa Indonesia telah menjadi pemersatu warga Negara Indonesia dan telah menjadi simbol kebanggaan masyarakat. Bahasa Indonesia sebagai bahasa negara telah diatur oleh UUD 1945 Pasal ...",
+    opt: ["35","36","36 A","36 B","36 D"],
+    a: 1,
     pembahasan: ""
   },
   { // No 20
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "ecara horizontal kebinekaan masyarakat Indonesia dapat dilihat dari ...",
+    opt: ["perbedaan kepemilikan kekayaan dan jenjang pendidikan","perbedaan kedudukan dan jabatan di tengah-tengah masyarakat","perbedaan ras, suku bangsa, serta agama.","perbedaan jenis kelamin, ras, dan tingkat pendidikan","perbedaan tingkat pendidikan, status sosial, dan kepemilikan lahan"],
+    a: 2,
     pembahasan: ""
   },
   { // No 21
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "abila di dalam masyarakat terdapat konsolidasi yang terdiri atas dua kelompok berlatar belakang etnis dan kebudayaan yang berbeda, maka akan berpotensi menimbulkan ...",
+    opt: ["integrasi sosial","diferensiasi sosial","konflik sosial","disintegrasi sosial","reintegrasi sosial"],
+    a: 1,
     pembahasan: ""
   },
   { // No 22
-    q: "",
-    opt: ["","","","",""],
+    q: "Pancasila memiliki kedudukan sebagai dasar negara yang memiliki arti ...",
+    opt: ["Pancasila sebagai lambang pemersatu masyarakat","Pancasila sebagai lambang persatuan dan kesatuan bangsa","Pancasila digunakan sebagai sumber informasi dan pengetahuan masyarakat.","Pancasila digunakan sebagai sumber tata kelakuan dan ketertiban masyarakat","Pancasila digunakan sebagai landasan untuk mengatur pemerintah dan penyelenggaraan negara"],
     a: 4,
     pembahasan: ""
   },
   { // No 23
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Tokoh yang kali pertama mengemukakan rumusan dasar Negara Indonesia adalah ...",
+    opt: ["Mr. Dr. Muh Yamin","Prof. Dr. Mr. Soepото","Ir. Soekarno","Drs. Moh. Hatta","Mr. Achmad Soebardjo"],
+    a: 0,
     pembahasan: ""
   },
   { // No 24
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Usulan dasar Negara Indonesia yang dikemukakan oleh Prof. Dr. Soepomo berisi tentang....",
+    opt: ["perikebangsaan, musyawarah, persatuan, kekeluargaan, dan keadilan sosial","perikemanusiaan, kebangsaan, ketuhanan, keseimbangan dan per kerakyatan","persatuan, kekeluargaan, keseimbangan lahir dan batin, musyawarah, dan keadilan sosial","kesejahteraan rakyat, persatuan, kekeluargaan, keseimbangan, dan musyawarah","ketuhanan, kesejahteraan rakyat, persatuan, kekeluargaan, dan keseimbangan"],
+    a: 2,
     pembahasan: ""
   },
   { // No 25
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Lima dasar negara yang diusulkan oleh Ir. Soekarno secara berurutan adalah ...",
+    opt: ["KetuhananYang Maha Esa, kesejahteraan sosial, mufakat atau demokrasi, internasionalisme atau perikemanusiaan, kebangsaan Indonesia","Ketuhanan Yang Maha Esa, kebangsaan Indonesia, kesejahteraan sosial, internasionalisme atau perikemanusiaan mufakat, atau demokrasi","Ketuhanan Yang Maha Esa, kebangsaan Indonesia, kesejahteraan sosial, mufakat atau demokrasi, internasionalisme atau perikemanusiaan","kebangsaan Indonesia, internasionalisme atau perikemanusiaan, mufakat atau demokrasi, kesejahteraan sosial, dan Ketuhanan Yang Maha Esa","kebangsaan Indonesia, internasionalisme atau perikemanusiaan, Ketuhanan Yang Maha Esa, kesejahteraan sosial, mufakat atau demokrasi"],
+    a: 3,
     pembahasan: ""
   },
   { // No 26
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Berikut beberapa nama tokoh yang termasuk dalam Panitia Sembilan, kecuali ...",
+    opt: ["Ir. Soekarno, Drs. Moh. Hatta, Abikusno Tjokrosujoso","K.H. Wachid Hasyim, Mr. Muh. Yamin, Mr. A.A. Maramis","Ki Bagus Hadikusumo, Oto Iskandar Dinata, Sutardjo Kartohadikusumo","H. Agus Salim, Ir. Soekarno, Drs. Moh. Hatta","Ki Bagus Hadikusumo, H. Agus Salim, Mr. Muh Yasmin"],
+    a: 2,
     pembahasan: ""
   },
   { // No 27
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Pancasila ditetapkan menjadi dasar falsafah atau ideologi Republik Indonesia pada ...",
+    opt: ["14 Juli 1945","18 Agustus 1945","10 Juli 1945","31 Mei 1945","29 Mei 1945"],
+    a: 1,
     pembahasan: ""
   },
   { // No 28
-    q: "",
-    opt: ["","","","",""],
+    q: "Rumusan Pancasila yang resmi terdapat dalam ...",
+    opt: ["Piagam Jakarta","teks Proklamasi","pidato Ir. Soekarno","sidang BPUPKI","Pembukaan UUD 1945"],
     a: 4,
     pembahasan: ""
   },
   { // No 29
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Makna Pancasila ditinjau dari dimensi teologis adalah ...",
+    opt: ["dimensi Pancasila yang menunjukkan bahwa pembangunan mempunyai citacita Proklamasi 1945","dimensi yang menunjukkan bahwa dalam Pancasila manusia dan martabat manusia mempunyai kedudukan yang sentral","dimensi yang menempatkan manusia tidak secara individualis","dimensi yang berisi nilai-nilai luhur","dimensi yang berisi cita-cita atau pandangan hidup"],
+    a: 0,
     pembahasan: ""
   },
   { // No 30
-    q: "",
-    opt: ["","","","",""],
-    a: 4,
+    q: "Secara terminologi Pancasila memiliki arti ...",
+    opt: ["lima asas dasar","dasar yang memiliki lima sandi","lima tata laku","lima yang utama","tingkah laku baik"],
+    a: 0,
     pembahasan: ""
   }
   ];
